e862a5fd7c61f073beb71b8ea6206e6e5341102c
RazTheCat <razthecat@posteo.net>
2023-12-31 12:30:23 -0500

    modified:   apps/desktops/gnome/dconf.nix 	modified:   flake.lock 	modified:   system/system-imports.nix

--- system/system-imports.nix
+++ system/system-imports.nix
@@ -7,12 +7,12 @@
     [ 
       ./hardware-configuration.nix
       ../apps/system-apps.nix
-      #./kernels/vanilla.nix
-      ./kernels/cachy.nix
+      ./kernels/vanilla.nix
+      #./kernels/cachy.nix
       #./kernels/cachy-sched-ext.nix
       ./modules/sysctl.nix
-      ./daemons/schedulers/ananicy-cpp.nix
-      #./daemons/schedulers/system76-scheduler.nix
+      #./daemons/schedulers/ananicy-cpp.nix
+      ./daemons/schedulers/system76-scheduler.nix
       ./daemons/tlp.nix
       #../apps/desktops/gnome/gnome.nix
       #../apps/desktops/plasma/plasma5.nix
