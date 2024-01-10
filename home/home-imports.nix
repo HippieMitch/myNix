580d4697aa66d68659e3dcce19c4e45d74f365a2
RazTheCat <razthecat@posteo.net>
2024-01-10 11:35:50 -0500

    modified:   flake.lock 	modified:   home/home-imports.nix 	home/home-manager/

--- home/home-imports.nix
+++ home/home-imports.nix
@@ -7,12 +7,14 @@ imports =
     [  
 
 # Home Applications
-       ../apps/home-apps.nix
+    ../apps/home-apps.nix
 
 # dconf
-       #../apps/desktops/gnome/dconf.nix
+    #../apps/desktops/gnome/dconf.nix
 
-# XFCE home-manager
-       #../apps/desktops/xfce/xfce-home.nix
+# home-manager
+    #../apps/desktops/xfce/xfce-home.nix
+    ./home-manager/fish.nix
+    ./home-manager/kitty.nix
     ];
 }
