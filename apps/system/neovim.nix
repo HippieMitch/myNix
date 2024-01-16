503f5e534bb04571c45e4bb2f8c419e7ade9d506
RazTheCat <razthecat@posteo.net>
2024-01-16 14:43:38 -0500

    modified:   apps/system/neovim.nix

--- apps/system/neovim.nix
+++ apps/system/neovim.nix
@@ -8,18 +8,18 @@
     configure = {
       customRC = ''
         set number
-	    set shiftwidth=4
-	    set tabstop=4
-	    set softtabstop=4
-	    set expandtab
-	    syntax on
-	    set mouse=v
-	    set mouse=a
-	    set showmatch
+        set shiftwidth=4
+        set tabstop=4
+        set softtabstop=4
+        set expandtab
+        syntax on
+        set mouse=v
+        set mouse=a
+        set showmatch
         set ignorecase
-	    set hlsearch
+        set hlsearch
         set incsearch
-	    set autoindent
+        set autoindent
         set clipboard=unnamedplus
         filetype plugin on
       '';
