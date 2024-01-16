9fad459d16a4b3dafcc02e2e02b5f7b466239a02
RazTheCat <razthecat@posteo.net>
2024-01-16 14:35:27 -0500

    new file:   apps/system/neovim.nix

---
+++ apps/system/neovim.nix
@@ -0,0 +1,29 @@
+# Neovim Configuration
+{ ... }:
+
+{
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    configure = {
+      customRC = ''
+        set number
+	    set shiftwidth=4
+	    set tabstop=4
+	    set softtabstop=4
+	    set expandtab
+	    syntax on
+	    set mouse=v
+	    set mouse=a
+	    set showmatch
+        set ignorecase
+	    set hlsearch
+        set incsearch
+	    set autoindent
+        set cc=80
+        set clipboard=unnamedplus
+        filetype plugin on
+      '';
+    };
+  };
+}
