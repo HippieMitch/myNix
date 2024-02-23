ad9a96497ff4abc981c0dc50c3da82451b2e3241
RazTheCat <razthecat@posteo.net>
2024-02-23 10:35:13 -0500

    More formatting.

--- system/desktops/plasma/plasma5.nix
+++ system/desktops/plasma/plasma5.nix
@@ -4,30 +4,30 @@
 
 {
   # Enable the KDE Plasma Desktop Environment
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
-   elisa
-  ];
-  services.xserver.displayManager.defaultSession = "plasmawayland";
-  programs.kdeconnect.enable = true;
+    services.xserver.displayManager.sddm.enable = true;
+    services.xserver.desktopManager.plasma5.enable = true;
+    environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+      elisa
+    ];
+    services.xserver.displayManager.defaultSession = "plasmawayland";
+    programs.kdeconnect.enable = true;
 
   # Portals
-  xdg.portal.enable = true;
+    xdg.portal.enable = true;
 
-  # Plasma Apps
-  environment.systemPackages = with pkgs; [
-   libsForQt5.filelight
-   libsForQt5.kate
-   libsForQt5.kcalc
-   libsForQt5.kpmcore
-   libsForQt5.kcalc
-   partition-manager
-   wayland-utils
+  # Plasma Packages
+    environment.systemPackages = with pkgs; [
+      libsForQt5.filelight
+      libsForQt5.kate
+      libsForQt5.kcalc
+      libsForQt5.kpmcore
+      libsForQt5.kcalc
+      partition-manager
+      wayland-utils
 
-  # Icon Themes
-   qogir-icon-theme
-   tela-icon-theme
+    # Icon Themes
+      qogir-icon-theme
+      tela-icon-theme
 
-   ];
+    ];
 }
