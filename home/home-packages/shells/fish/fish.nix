cbfc59c2320ab29bdb38f383d3e8e20d53278e5e
RazTheCat <razthecat@posteo.net>
2024-03-26 23:11:00 -0400

    Proper dconf size fix.

--- home/home-packages/shells/fish/fish.nix
+++ home/home-packages/shells/fish/fish.nix
@@ -28,8 +28,8 @@
 	ncheck = "cd /etc/nixos && sudo nixos-rebuild dry-run";
     ndryrun = ''cd /etc/nixos && sudo nix build ".#nixosConfigurations.$(hostname).config.system.build.toplevel" && nvd diff /run/current-system ./result'';
 	fupd = "cd /etc/nixos && sudo nix flake update";
-	nswitch = "sudo nixos-rebuild switch --flake /etc/nixos/#spaceghost";
-	nboot = "sudo nixos-rebuild boot --flake /etc/nixos/#spaceghost";
+	nswitch = "nh os switch --ask /etc/nixos";
+	nboot = "nh os boot --ask /etc/nixos";
 	nrun = "nix-shell -p";
 	ngens = "sudo nix-env -p /nix/var/nix/profiles/system --list-generations";
 	nclean = "sudo nix-collect-garbage -d && nix-collect-garbage -d";
@@ -39,7 +39,7 @@
 	ls = "lsd";
 	ndev = "nix develop";
 	nsea = "nix-search";
-	dup = "dconf dump / | dconf2nix > dconf.nix && sudo cp /home/sean/dconf.nix /etc/nixos/system/system-packages/desktops/gnome/dconf.nix && rm /home/sean/dconf.nix";
+	dup = "dconf dump / | dconf2nix > dconf.nix && sudo cp /home/sean/dconf.nix /etc/nixos/system/system-packages/desktops/gnome/dconf/dconf.nix && rm /home/sean/dconf.nix";
 	kconf = "nvim ~/.config/kitty/kitty.conf";
 	pkgver = "nvd list | grep";
 	streamhop = "sudo nix flake update --override-input nixpkgs github:NixOS/nixpkgs";
