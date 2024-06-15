0bfbacfff815f887db55d695d41469670fdce7ab
RazTheCat <razthecat@posteo.net>
2024-06-15 12:47:55 -0400

    sysctl changes.

--- system/modules/dns.nix
+++ system/modules/dns.nix
@@ -1,14 +1,60 @@
-# Use CloudFlare DNS
+# Use Stubby and Quad9 DNS
 
 { config, ... }:
 
 {
-  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
-
-  services.resolved = {
-    enable = true;
-    domains = [ "~." ];
-    fallbackDns = [ "1.1.1.1" "1.0.0.1" ];
-    dnsovertls = "true";
+  ## DNS-over-TLS
+services.stubby = {
+  enable = true;
+  settings = {
+    listen_addresses = [ "127.0.0.1" "0::1" ];
+    resolution_type = "GETDNS_RESOLUTION_STUB";
+    dns_transport_list = [ "GETDNS_TRANSPORT_TLS" ];
+    tls_authentication = "GETDNS_AUTHENTICATION_REQUIRED";
+    tls_query_padding_blocksize = 128;
+    idle_timeout = 10000;
+    round_robin_upstreams = 1;
+    tls_min_version = "GETDNS_TLS1_3";
+    dnssec = "GETDNS_EXTENSION_TRUE";
+    upstream_recursive_servers = [
+      {
+        address_data = "9.9.9.9";
+        tls_auth_name = "dns.quad9.net";
+      }
+      {
+        address_data = "149.112.112.112";
+        tls_auth_name = "dns.quad9.net";
+      }
+      {
+        address_data = "2620:fe::fe";
+        tls_auth_name = "dns.quad9.net";
+      }
+      {
+        address_data = "2620:fe::9";
+        tls_auth_name = "dns.quad9.net";
+      }
+      {
+        address_data = "1.1.1.2";
+        tls_auth_name = "cloudflare-dns.com";
+      }
+      {
+        address_data = "1.0.0.2";
+        tls_auth_name = "cloudflare-dns.com";
+      }
+      {
+        address_data = "2606:4700:4700::1112";
+        tls_auth_name = "cloudflare-dns.com";
+      }
+      {
+        address_data = "2606:4700:4700::1002";
+        tls_auth_name = "cloudflare-dns.com";
+      }
+    ];
+  };
+};
+networking.nameservers = [ "::1" "127.0.0.1" ];
+services.resolved = {
+  enable = true;
+  fallbackDns = [ "2620:fe::fe" "2620:fe::9" "9.9.9.9" "149.112.112.112" ];
   };
 }
--- system/modules/sysctl.nix
+++ system/modules/sysctl.nix
@@ -4,8 +4,6 @@
 
 {
   boot.kernel.sysctl = {
-    "net.core.default_qdisc" = "fq_codel";
-    "net.ipv4.tcp_congestion_control" = "bbr";
     "vm.swappiness" = 100;
     "vm.vfs_cache_pressure" = 50;
     "vm.dirty_bytes" = 268435456;
@@ -21,25 +19,14 @@
     "kernel.kptr_restrict" = 2;
     "kernel.kexec_load_disabled" = 1;
     "kernel.sched_rt_runtime_us" = 980000;
-    "net.core.netdev_max_backlog" = 16384;
-    "net.core.somaxconn" = 8192;
-    "net.core.rmem_default" = 1048576;
-    "net.core.rmem_max" = 16777216;
-    "net.core.wmem_default" = 1048576;
-    "net.core.wmem_max" = 16777216;
-    "net.core.optmem_max" = 65536;
-    "net.ipv4.tcp_rmem" = "4096 1048576 3276800";
-    "net.ipv4.tcp_wmem" = "4096 65536 16777216";
-    "net.ipv4.udp_rmem_min" = 8192;
-    "net.ipv4.udp_wmem_min" = 8192;
-    "net.ipv4.tcp_fastopen" = 3;
-    "net.ipv4.tcp_syncookies" = 1;
-    "net.ipv4.tcp_rfc1337" = 1;
-    "net.ipv4.tcp_ecn" = 1;
-    "net.ipv4.tcp_timestamps" = 0;
-    "net.ipv4.tcp_slow_start_after_idle" = 0;
+    "fs.inotify.max_user_watches" = 524288;
+    "fs.file-max" = 2097152;
+    "kernel.sysrq" = 0;
+    "net.ipv4.icmp_echo_ignore_broadcasts" = 1;
+    "net.ipv4.icmp_ignore_bogus_error_responses" = 1;
     "net.ipv4.conf.default.rp_filter" = 1;
     "net.ipv4.conf.all.rp_filter" = 1;
+    "net.ipv4.tcp_syncookies" = 1;
     "net.ipv4.conf.all.accept_redirects" = 0;
     "net.ipv4.conf.default.accept_redirects" = 0;
     "net.ipv4.conf.all.secure_redirects" = 0;
@@ -47,8 +34,11 @@
     "net.ipv6.conf.all.accept_redirects" = 0;
     "net.ipv6.conf.default.accept_redirects" = 0;
     "net.ipv4.conf.all.send_redirects" = 0;
-    "net.ipv4.conf.default.send_redirects" = 0;
-    "fs.inotify.max_user_watches" = 524288;
-    "fs.file-max" = 2097152;
+    "net.ipv4.conf.all.accept_source_route" = 0;
+    "net.ipv6.conf.all.accept_source_route" = 0;
+    "net.ipv4.tcp_rfc1337" = 1;
+    "net.ipv4.tcp_fastopen" = 3;
+    "net.ipv4.tcp_congestion_control" = "bbr";
+    "net.core.default_qdisc" = "cake";
   };
 }
